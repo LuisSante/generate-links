f06bffdb10bee15e55bc2a34613cca3adcadedd4
LuisSante <C3012708@fgv.edu.br>
2026-09-03 17:34:00 -0300

    image

--- links_scraper/links_scraper/spiders/projudi.py
+++ links_scraper/links_scraper/spiders/projudi.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from PIL import Image
+from io import BytesIO
+import pytesseract
 
 import scrapy
 
@@ -12,7 +15,7 @@ class ProjudiSpider(scrapy.Spider):
         main_src = response.xpath('//frame[@name="mainFrame"]/@src').get()
         yield response.follow(main_src, callback=self.parse_main)
 
-    def parse_main(self, response):
+    def parse_json(self, response):
         Path("main_page.html").write_bytes(response.body)
 
         form = response.css("form#formConsultaPublica")
@@ -27,4 +30,36 @@ class ProjudiSpider(scrapy.Spider):
             "captcha_img": response.urljoin(
                 response.css("#idImg").attrib["src"]
             ),
+        }
+
+    def parse_main(self, response):
+        form = response.css("form#formConsultaPublica")
+        numero_input = form.css("input#numeroProcesso")
+
+        bck_img = response.urljoin(
+            response.css("#idImg").attrib["src"]
+        )
+
+        yield scrapy.Request(
+            bck_img,
+            callback=self.parse_captcha,
+            meta={
+                "bck_img": bck_img,
+                "form_action": response.urljoin(form.attrib["action"]),
+                "input_name": numero_input.attrib["name"],
+            },
+        )
+
+    def parse_captcha(self, response):
+        image = Image.open(BytesIO(response.body))
+
+        # text = pytesseract.image_to_string(image, config="--psm 8")
+        # text = pytesseract.image_to_string(image, config="--psm 8 --oem 3 -c tessedit_char_whitelist=0123456789")
+        # text = pytesseract.image_to_string(image, config="--psm 8 --oem 3 -c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        # text = pytesseract.image_to_string(image, config="--psm 8 --oem 3 -c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+        text = pytesseract.image_to_string(image)
+
+        yield {
+            "bck_img": response.meta["bck_img"],
+            "captcha_text": text.strip(),
         }
